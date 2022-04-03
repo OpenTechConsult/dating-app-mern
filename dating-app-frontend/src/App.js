7bdd6fbdc9f51d859d708ca8a522d6bb85493a88
OpenTech-Consult <opentech.consult@gmail.com>
2022-04-03 23:50:23 +0000

    Dating cards components
    created dating card component inside de components folder in the src directory.
    we installed and used the react-tinder-card component for that
    
    closes #5

--- dating-app-frontend/src/App.js
+++ dating-app-frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DatingCards from "./components/datingcards.component";
 import Header from "./components/header.component";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DatingCards />
     </div>
   );
 }
