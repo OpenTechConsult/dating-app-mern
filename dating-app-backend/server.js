36d24d28f79c684d1eaf0049bea4c920a75e3394
OpenTech-Consult <opentech.consult@gmail.com>
2022-04-07 13:00:12 +0000

    Database user and network access
    created database user and provide network access
    created a connection url and use mongoose to connect to Mongodbatlas database.
    
    Closes #17

--- dating-app-backend/server.js
+++ dating-app-backend/server.js
@@ -4,10 +4,15 @@ import mongoose from "mongoose";
 // app config
 const app = express();
 const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://eagboka:11JTvFlzDBiSf4GD@datingappmerncluster.ujuus.mongodb.net/datingmernappdb?retryWrites=true&w=majority';
 
 // middleware
 
 // db config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
